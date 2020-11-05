1609717e092138ca44f89c1eb112ee3372a0f8a2
jizhekai <jizhekai@126.com>
2020-11-05 22:19:58 +0800

    基本完成脚本功能

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import yaml
 import csv
+import os
 
-def readYaml(howTo):
+def readYaml():
     file_path  = './data/data.yaml'
-    excel_path = './data/data.xlsx'
-    with open(file_path,'r',encoding='utf-8') as f:
-        data = yaml.load(f,yaml.FullLoader)
-        with open('./data/data.csv','a+') as csv_f:
-            for caseList in data["data"]:
-                csv_f.write('\n')
-                csv_f.writelines([caseList[item] for item in caseList])
+    with open(file_path,'r',encoding='utf-8-sig') as f:
+        data = yaml.load(f,yaml.FullLoader)['data']
+        lines = []
+        for case in data:
+            line = []
+            for item in case:
+                if isinstance(case[item],list):
+                    case[item] = '\n'.join(['【'+ str(case[item].index(line)+1) +'】'+ line for line in case[item]])
+                line.append(case[item])
+            lines.append(line)
     f.close()
+            
+    fcsv = open('./data/data.csv','a+',encoding='utf-8-sig',newline="")
+    f_csv = csv.writer(fcsv)
+    for line in lines:
+        f_csv.writerow(line)
 
 if __name__ == '__main__':
-    readYaml('new')
+    readYaml()
