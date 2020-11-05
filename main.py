c5a7a81eb5c95de3bead135cacecdda455da1c8d
jizhekai <jizhekai@126.com>
2020-11-05 18:04:50 +0800

    read yaml

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import yaml
+
+def readYaml():
+    file_path  = './data/data.yaml'
+    with open(file_path,'r',encoding='utf-8') as f:
+        data = yaml.load(f,yaml.FullLoader)
+        print(data)
+    f.close()
+
 if __name__ == '__main__':
-    pass
+    readYaml()
